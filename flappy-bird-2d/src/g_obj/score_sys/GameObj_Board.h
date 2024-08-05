cc52815e87a20226d4d68b6d677d1242c401870c
4tom <antek0211@gmail.com>
2024-08-05 12:01:47 +0200

    mod #12

---
+++ flappy-bird-2d/src/g_obj/score_sys/GameObj_Board.h
@@ -0,0 +1,15 @@
+#ifndef _GAMEOBJ_BOARD_
+#define _GAMEOBJ_BOARD_
+
+#include <flappy_engine.h>
+
+namespace flappy_bird_2D {
+
+	class GameObj_Board : public flappy_engine::GameObj
+	{
+		public:
+			GameObj_Board();
+	};
+}
+
+#endif
