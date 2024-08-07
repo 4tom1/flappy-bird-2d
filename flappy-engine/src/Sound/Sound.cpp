54abfc037650013874ae94a213c84da239c99506
4tom <antek0211@gmail.com>
2024-08-07 17:39:49 +0200

    mod #11

--- flappy-engine/src/Sound/Sound.cpp
+++ flappy-engine/src/Sound/Sound.cpp
@@ -1,1 +1,11 @@
 #include "Sound.h"
+
+void flappy_engine::Sound::PlaySound(const char* file_name)
+{
+	if (music.openFromFile(file_name)) music.play();
+}
+
+void flappy_engine::Sound::SetVolume(float volume)
+{
+	music.setVolume(volume);
+}
--- flappy-engine/src/Sound/Sound.h
+++ flappy-engine/src/Sound/Sound.h
@@ -1,5 +1,21 @@
-#pragma once
-class Sound
-{
-};
+#ifndef _FLAPPY_ENGINE_SOUND_
+#define _FLAPPY_ENGINE_SOUND_
 
+#include <SFML/Audio.hpp>
+
+namespace flappy_engine {
+
+	class Sound
+	{
+		public:
+
+			void PlaySound(const char*);
+			void SetVolume(float);
+
+		private:
+			
+			sf::Music music;
+	};
+}
+
+#endif
