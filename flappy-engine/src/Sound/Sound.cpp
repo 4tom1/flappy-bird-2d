c1300ef4eae6446aea133a8acfb16381ba0352ff
4tom <antek0211@gmail.com>
2024-08-07 18:03:28 +0200

    mod #12

--- flappy-engine/src/Sound/Sound.cpp
+++ flappy-engine/src/Sound/Sound.cpp
@@ -1,8 +1,18 @@
+#include <iostream>
+
 #include "Sound.h"
 
 void flappy_engine::Sound::PlaySound(const char* file_name)
 {
-	if (music.openFromFile(file_name)) music.play();
+	if (!music.openFromFile(file_name))
+	{
+		std::cout << "Error: Cannot open a file." << std::endl;
+		std::getchar();
+
+		return;
+	}
+		
+	music.play();
 }
 
 void flappy_engine::Sound::SetVolume(float volume)
