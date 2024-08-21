c4ff4f7cfbed540b70eba71a03d6731f2e3e82f6
4tom <antek0211@gmail.com>
2024-08-21 14:19:03 +0200

    mod #50 Micro game

--- sandbox/src/Main.cpp
+++ sandbox/src/Main.cpp
@@ -29,20 +29,14 @@ struct Player : public flappy_engine::GameObj
 		AddComponent(flappy_engine::sprite);
 		AddComponent(flappy_engine::collider);
 
-		transform.SetScale(2.f);
+		transform.SetScale(-1, 1);
 		sprite->Create(BIRD_IMG);
 		collider->SetSize(50.f);
 
 		transform.position.z = 3.f;
 	}
 
-	void Update()
-	{
-		if (direction && transform.scale.x < 0) sprite->FlipX();
-		if (!direction && transform.scale.x > 0) sprite->FlipY();
-	}
-
-	bool direction = false;
+	bool direction = true;
 };
 
 struct Coin : public flappy_engine::GameObj
@@ -58,6 +52,8 @@ struct Coin : public flappy_engine::GameObj
 
 		transform.position.z = 2.f;
 	}
+
+	bool picked_up = true;
 };
 
 struct Points : public flappy_engine::GameObj
@@ -82,7 +78,7 @@ struct Background : public flappy_engine::GameObj
 	{
 		AddComponent(flappy_engine::sprite);
 
-		transform.SetScale(2.f, 2.2f);
+		transform.SetScale(2.2f, 1.8f);
 		sprite->Create(BACKGROUND_IMG);
 
 		transform.position.z = 1.f;
@@ -91,33 +87,19 @@ struct Background : public flappy_engine::GameObj
 
 struct GameManager : public flappy_engine::GameObj
 {
-	GameManager()
-	{
-		background = new Background();
-		player = new Player();
-		points = new Points();
-	}
-
-	~GameManager()
-	{
-		delete background;
-		delete player;
-		delete points;
-	}
-	
 	void Update()
 	{
 		for (size_t i = 0; i < COIN_LIMIT; i++)
 		{
 			if (coin_array[i].collider->IsTriggered())
 			{
-				coin_array[i].transform.position.z = 0.f;
-			}
-		}
+				coin_array[i].picked_up = true;
+				points++;
 
-		for (size_t i = 0; i < COIN_LIMIT; i++)
-		{
-			if (coin_array[i].transform.position.z == 0.f)
+				std::cout << points.points << ' ';
+			}
+			
+			if (coin_array[i].picked_up)
 			{
 				float x;
 				float y;
@@ -125,45 +107,44 @@ struct GameManager : public flappy_engine::GameObj
 				x = rand() % WINDOW_WIDTH;
 				y = rand() % WINDOW_HIGH;
 
-				coin_array[i].transform.SetPosition(x, y, 2.f);
+				coin_array[i].transform.SetPosition(x, y);
+				coin_array[i].picked_up = false;
 			}
 		}
 	}
 
 	Coin coin_array[COIN_LIMIT];
-	Points* points;
-	Background* background;
-	Player* player;
+	Points points;
+	Background background;
+	Player player;
 };
 
 int main()
 {	
 	flappy_engine::Engine* engine = flappy_engine::InitEngine(WINDOW_WIDTH, WINDOW_HIGH, FRAME_RATE, "Flappy Bird", 100);
 
-	GameManager game_manager;
+	GameManager* game_manager = new GameManager();
 
 	while (engine->window.isOpen())
 	{
 		if (engine->input.IsKeyPressed(flappy_engine::UP))
 		{
-			game_manager.player->transform.position.y -= SPEED / FRAME_RATE;
+			game_manager->player.transform.position.y -= SPEED / FRAME_RATE;
 		}
 			
 		if (engine->input.IsKeyPressed(flappy_engine::DOWN))
 		{
-			game_manager.player->transform.position.y += SPEED / FRAME_RATE;
+			game_manager->player.transform.position.y += SPEED / FRAME_RATE;
 		}
 			
 		if (engine->input.IsKeyPressed(flappy_engine::LEFT))
 		{
-			game_manager.player->transform.position.x -= SPEED / FRAME_RATE;
-			game_manager.player->direction = false;
+			game_manager->player.transform.position.x -= SPEED / FRAME_RATE;
 		}
 		
 		if (engine->input.IsKeyPressed(flappy_engine::RIGHT))
 		{
-			game_manager.player->transform.position.x += SPEED / FRAME_RATE;
-			game_manager.player->direction = true;
+			game_manager->player.transform.position.x += SPEED / FRAME_RATE;
 		}
 
 		engine->UpdateAll();
