ffe091c9dd99796b55eebfb948dfd55d6263ce60
4tom <antek0211@gmail.com>
2024-08-31 23:57:21 +0200

    mod #19 works!

--- flappy-bird-2d/src/Settings.h
+++ flappy-bird-2d/src/Settings.h
@@ -14,12 +14,12 @@
 #define HIGHT_LIMIT -50
 
 // Bird settings
-#define BIRD_POS_Z 2
+#define BIRD_POS_Z 4
 #define BIRD_SPEED 200 / FRAME_RATE
 #define BIRD_ACCELERATION_CHANGE 0.1
 #define BIRD_ANIM_FRAME_RATE 12
 #define BIRD_COLLIDER_WIDTH 1 // % proc.
-#define BIRD_COLLIDER_HIGHT 1
+#define BIRD_COLLIDER_HIGHT 1 // % proc.
 // Start
 #define BIRD_START_JUMP_SPEED 30 / FRAME_RATE
 #define BIRD_START_ACCELERATION 1
